513e9e9819c6a412deff4081c48cf8cbc072a380
Oliver Newland <oliver.newland@crowdflower.com>
2015-12-22 08:49:10 -0800

    More cleanup

--- src/main/java/com/oliverco/problems/DuelBulldozer.java
+++ src/main/java/com/oliverco/problems/DuelBulldozer.java
@@ -12,9 +12,10 @@ import java.util.HashMap;
 import java.util.Set;
 
 public class DuelBulldozer {
-    final String venue = "KHJEX";
-    final String ticker = "EQXO";
-    final String tradingAccount = "HAI5175137";
+    public static final int ASK_BID_DELTA = 50;
+    final String venue = "KUTEX";
+    final String ticker = "BSH";
+    final String tradingAccount = "KFB61990514";
     final String apiKey = Environment.getenv("API_KEY");
     private final HashMap<Integer, ServerOrderStatus> openOrders = Maps.newHashMap();
     private int netShares = 0;
@@ -22,6 +23,9 @@ public class DuelBulldozer {
     public void execute(CloseableHttpClient httpClient) throws URISyntaxException, IOException, InterruptedException {
         ClientWrapper httpWrapper = new ClientWrapper(httpClient, apiKey);
         int position = 0;
+        int exposure = 0;
+        int totalSold = 0;
+        int totalBought = 0;
 
         OrderRequest buyOrderRequest = new OrderRequest();
         buyOrderRequest.account = tradingAccount;
@@ -38,26 +42,32 @@ public class DuelBulldozer {
         sellOrderRequest.orderType = "limit";
 
         for(int i = 0; i < 500; i++) {
-            System.out.println(netShares + " shares");
-            System.out.println(openOrders.size() + " open orders");
-            System.out.println("position = " + position);
+            System.out.println();
+            System.out.println(
+                    String.format(
+                            "shares %5d open %5d position %4d exposure %4d fbuy %8d fsell %8d",
+                            netShares,
+                            openOrders.size(),
+                            position,
+                            exposure,
+                            totalBought,
+                            totalSold));
 
             ServerOrderbook serverOrderbook = httpWrapper.getOrderbook(venue, ticker);
 
-            if(serverOrderbook.asks != null &&
-                serverOrderbook.bids != null)
-            {
+            if(serverOrderbook.asks != null && serverOrderbook.bids != null) {
                 OrderStatistics askStats = OrderStatistics.fromList(serverOrderbook.asks);
                 OrderStatistics bidStats = OrderStatistics.fromList(serverOrderbook.bids);
 
                 System.out.println("asks: " + askStats);
                 System.out.println("bids: " + bidStats);
 
-                if(askStats.getAvgOrder() - bidStats.getAvgOrder() > 10) {
+                if(askStats.getAvgOrder() - bidStats.getAvgOrder() > ASK_BID_DELTA) {
                     if(position < 300) {
                         // buy 10 cents above average bid
                         buyOrderRequest.qty = 100;
                         buyOrderRequest.price = bidStats.getAvgOrder() + 10;
+                        exposure += 100;
                         ServerOrderStatus result = httpWrapper.placeOrder(buyOrderRequest);
                         if (result.ok) {
                             openOrders.put(result.id, result);
@@ -70,6 +80,7 @@ public class DuelBulldozer {
                         // sell 10 cents below average ask
                         sellOrderRequest.qty = 100;
                         sellOrderRequest.price = askStats.getAvgOrder() - 10;
+                        exposure += 100;
                         ServerOrderStatus result = httpWrapper.placeOrder(sellOrderRequest);
                         if (result.ok) {
                             openOrders.put(result.id, result);
@@ -85,7 +96,7 @@ public class DuelBulldozer {
                 System.out.println("asks or bids were null, skipping iteration");
             }
 
-            Thread.sleep(500);
+            Thread.sleep(2000);
 
             netShares = 0;
             position = 0;
@@ -102,16 +113,23 @@ public class DuelBulldozer {
                         fillTotal += f.qty;
                     }
 
+                    exposure -= fillTotal;
+
                     if(newOrderStatus.direction.equals("buy")) {
                         netShares += newOrderStatus.qty;
                         position += fillTotal;
+                        totalBought += newOrderStatus.price * newOrderStatus.qty;
                     } else if(newOrderStatus.direction.equals("sell")) {
                         netShares -= newOrderStatus.qty;
                         position -= fillTotal;
+                        totalSold += newOrderStatus.price * newOrderStatus.qty;
                     }
 
                     openOrders.put(status.id, newOrderStatus);
                     if(status.qty == fillTotal || !newOrderStatus.open) {
+                        System.out.println(
+                                String.format("[fill] %s %d at %d", newOrderStatus.direction, fillTotal, newOrderStatus.price)
+                        );
                         idsToRemove.add(newOrderStatus.id);
                     }
                 }
